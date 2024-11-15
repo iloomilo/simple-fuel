be2881617cc58f4616674f01d2b7851fb5d0cbc7
Ilcan Tiryaki <154640529+iloomilo@users.noreply.github.com>
2024-11-15 06:01:21 +0100

    added check for deleted/destroyed vehicles

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import * as alt from 'alt-server';
 import { useRebar } from '@Server/index.js';
-import { FuelConfig } from './shared/config.js';
+import { FuelConfig } from '../shared/config.js';
 
 let activeVehicles = new Set();
 const Rebar = useRebar();
@@ -17,7 +17,8 @@ declare module 'alt-server' {
 SyncBinder.syncVehicleKey('fuel');
 
 //To only iterate through active vehicles
-alt.on('vehicle:motor-state-change', (veh: alt.Vehicle) => {
+// You need to emit this event on your engine start/stop hotkey/command
+alt.on('vehicle:engine-state-change', (veh: alt.Vehicle) => {
     if (veh.valid && veh.engineOn) {
         activeVehicles.add(veh);
     } else {
@@ -27,9 +28,18 @@ alt.on('vehicle:motor-state-change', (veh: alt.Vehicle) => {
 
 //Set fuelCounter at vehicleBound
 alt.on('rebar:vehicleBound', (veh: alt.Vehicle) => veh.setMeta(fuelSessionKey, 0));
+//remove vehicle, if it gets destroyed
+alt.on('vehicleDestroy', (veh: alt.Vehicle) => {
+    alt.log(activeVehicles);
+    activeVehicles.delete(veh);
+});
 
 setInterval(() => {
     activeVehicles.forEach((veh: alt.Vehicle) => {
+        // remove vehicle, if entity got deleted by someone
+        if (!veh.valid) {
+            activeVehicles.delete(veh);
+        }
         let vehDoc = Rebar.document.vehicle.useVehicle(veh);
         let fuel = vehDoc.getField('fuel');
         let fuelCounter = veh.getMeta(fuelSessionKey);
@@ -39,6 +49,15 @@ setInterval(() => {
             activeVehicles.delete(veh);
             return;
         }
+
+        if (fuel === 20) {
+            const players = Rebar.get.usePlayersGetter().inVehicle(veh);
+            players.forEach((player) => {
+                alt.log(player);
+                Rebar.player.useAudio(player).playSound('http://assets/sounds/fuel.ogg');
+            });
+        }
+
         //  add interval seconds to fuelCounter
         veh.setMeta(fuelSessionKey, (fuelCounter += FuelConfig.checkInterval));
         // subtract one fuel unit, if fuelCounter reached limit
